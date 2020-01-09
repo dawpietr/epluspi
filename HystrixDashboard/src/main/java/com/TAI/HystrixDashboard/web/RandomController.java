bffb7332888968da5754af7201e9c5b0925ed15a
dawpietr <50321773+dawpietr@users.noreply.github.com>
2020-01-09 19:59:38 +0100

    Add files via upload

---
+++ HystrixDashboard/src/main/java/com/TAI/HystrixDashboard/web/RandomController.java
@@ -0,0 +1,26 @@
+package com.TAI.HystrixDashboard.web;
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
+
+@RestController
+class RandomController {
+
+    @GetMapping("/")
+    @HystrixCommand(fallbackMethod = "fallback")
+    String getRandomNumber() throws InterruptedException {
+        int result = new Random().nextInt(10);
+        if (result > 5) {
+            throw new RuntimeException("¯\\_(ツ)_/¯");
+        }
+        return String.format("The number is {%d}", result);
+    }
+
+    String fallback(){
+        return "Hystrix is working!";
+    }
+
+}
